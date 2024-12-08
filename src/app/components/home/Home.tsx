6e7c7d6f085b8f649d4bdc9d037767e5b193a8f1
Orestarxx <oresto322@gmail.com>
2024-12-08 23:20:01 +0200

    done

--- src/app/components/home/Home.tsx
+++ src/app/components/home/Home.tsx
@@ -1,49 +1,57 @@
 import React from 'react';
 import styles from './home.style.module.css'
-import {sortedMoviesAndTV} from "@/app/helpers/sortMoviesAndFilms";
 import {imgOriginal} from "@/app/urls/urls";
 import ButtonForMainPoster from "@/app/components/home/buttonForMainPoster/ButtonForMainPoster";
 import CarouselForTVsAndMovies from "@/app/components/carousel/carouselForTVsAndMovies/CarouselForTVsAndMovies";
+import {movieService} from "@/app/services/movie.service";
+import {tvService} from "@/app/services/tv.service";
 
 const Home = async () => {
-    const sorted = await sortedMoviesAndTV();
+    const promised = await Promise.all([
+         movieService.movies.getPopularMovies('1'),
+         movieService.movies.getNowPlaying('1'),
+         movieService.movies.getTopRated('1'),
+         tvService.tvSeries.getPopularTV('1'),
+         tvService.tvSeries.getAiringToday('1'),
+         tvService.tvSeries.getTopRatedTV('1')
+    ])
 
     return (
         <div id={styles.home}>
             <div id={styles.topRatedHolder}>
                 <div id={styles.infoBigPoster}>
                     <div id={styles.leftSide}>
-                        <ButtonForMainPoster id={sorted.topRatedMovies[0].id}/>
+                        <ButtonForMainPoster id={promised && promised[2].results[0].id}/>
                     </div>
                     <div id={styles.rightSide}>
-                        <h2>{sorted.topRatedMovies[0].title}</h2>
+                        <h2>{promised[2].results[0].title}</h2>
                     </div>
                 </div>
-                <img src={imgOriginal + sorted.topRatedMovies[0].backdrop_path} alt=""/>
+                <img src={imgOriginal + promised[2].results[0].backdrop_path} alt=""/>
             </div>
             <div id={styles.carouselHolder}>
                 <div className={styles.cardsHolder}>
-                    <CarouselForTVsAndMovies array={sorted.popularMovies}
+                    <CarouselForTVsAndMovies array={promised[0].results}
                                              pathAndName={{path: '/movies/', name: 'Popular Movies'}}/>
                 </div>
                 <div className={styles.cardsHolder}>
-                    <CarouselForTVsAndMovies array={sorted.popularTV}
+                    <CarouselForTVsAndMovies array={promised[3].results}
                                              pathAndName={{path: '/tvseries/', name: 'Popular TV Shows'}}/>
                 </div>
                 <div className={styles.cardsHolder}>
-                    <CarouselForTVsAndMovies array={sorted.topRatedMovies}
+                    <CarouselForTVsAndMovies array={promised[2].results}
                                              pathAndName={{path: '/movies/', name: 'Top Rated Movies'}}/>
                 </div>
                 <div className={styles.cardsHolder}>
-                    <CarouselForTVsAndMovies array={sorted.topRatedTV}
+                    <CarouselForTVsAndMovies array={promised[5].results}
                                              pathAndName={{path: '/tvseries/', name: 'Top Rated TV Shows'}}/>
                 </div>
                 <div className={styles.cardsHolder}>
-                    <CarouselForTVsAndMovies array={sorted.nowPlaying}
+                    <CarouselForTVsAndMovies array={promised[1].results}
                                              pathAndName={{path: '/movies/', name: 'Now Playing Movies'}}/>
                 </div>
                 <div className={styles.cardsHolder}>
-                    <CarouselForTVsAndMovies array={sorted.airingToday}
+                    <CarouselForTVsAndMovies array={promised[4].results}
                                              pathAndName={{path: '/tvseries/', name: 'Airing Today TV Shows'}}/>
                 </div>
             </div>
--- src/app/data/data.ts
+++ src/app/data/data.ts
@@ -1,4 +1,4 @@
 export const headers = {
         accept: 'application/json',
-        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1NmVmOTEyMGFlZGRmMWNiOGEyOTZhNGIwM2E3YzMwZSIsIm5iZiI6MTczMTY2Njg1OS42NTEzOTQxLCJzdWIiOiI2NmU5NTJkODgyZmY4NzNmN2QxZWE1OGUiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.bj2hm7z7JVIPj3IEIi6hNj3VxZIGyuaK0ui91y6Osrg'
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwODc0YTc3NjE3MWRlMzI0ZTk2Nzc4MDNhNzVhZTcxOCIsIm5iZiI6MTcyNjU2NzEyOC42OTcsInN1YiI6IjY2ZTk1MmQ4ODJmZjg3M2Y3ZDFlYTU4ZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.4kBNK41hRBlUDlOgCbja0__IZ5oZ04DY40xCmMMv_Rs'
 }
